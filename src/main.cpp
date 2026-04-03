db1366ce9a556bab1b10b75429721b2b901a8764
Matej Tomco <140529551+kolihummingbird@users.noreply.github.com>
2026-04-03 22:58:55 +0200

    Added monte carlo antithetic, formatted output, confidence interval, delta basic design

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
-#include<iostream>
+#include <iostream>
 #include <random>
 #include <cmath>
+#include <iomanip>
 
 using namespace std;
 
@@ -37,6 +38,26 @@ double black_scholes_call(double S, double K, double r, double sigma, double T)
 }
 
 
+double monte_carlo_call_antithetic(int num_paths, double S,
+	double K, double r, double sigma, double T)
+{
+	double payoff_sum = 0.0;
+	for (int i = 0; i < num_paths / 2 ; ++i)
+	{
+		double Z = normal_random();
+		
+		double ST1 = S * std::exp((r - 0.5 * sigma * sigma) * T + sigma * std::sqrt(T) * Z);
+		double ST2 = S * std::exp((r - 0.5 * sigma * sigma) * T - sigma * std::sqrt(T) * Z);
+		
+		double payoff1 = std::max(ST1-K,0.0);
+		double payoff2 = std::max(ST2 - K, 0.0);
+	
+		payoff_sum += (payoff1 + payoff2);
+	}
+	return std::exp(-r * T) * (payoff_sum / num_paths);
+
+}
+
 
 double simulate_path(double S0, double mu, double sigma, double T, int steps)
 {
@@ -66,6 +87,43 @@ double monte_carlo_call(int num_paths, double S, double K, double r, double sigm
 	return std::exp(-r * T) * (payoff_sum / num_paths);
 }
 
+double monte_carlo_call_stats(int num_paths, double S, 
+	double K, double r, double sigma, 
+	double T, double& std_error)
+{
+	double sum = 0.0;
+	double sum_sq = 0.0;
+
+	for (int i = 0; i < num_paths; ++i)
+	{
+		double Z = normal_random();
+
+		double ST = S * std::exp((r - 0.5 * sigma * sigma) * T
+			+ sigma * std::sqrt(T) * Z);
+
+		double payoff = std::max(ST - K, 0.0);
+
+		sum += payoff;
+		sum_sq += payoff * payoff;
+	}
+	double mean = sum / num_paths;
+
+	double variance = (sum_sq / num_paths) - (mean * mean);
+	std_error = std::sqrt(variance / num_paths);
+
+	return std::exp(-r * T) * mean;
+}
+
+double monte_carlo_delta(int paths, double S, double K, double r, double sigma, double T) 
+{
+	double eps = 0.01 * S;
+
+	double price_up = monte_carlo_call(paths, S + eps, K, r, sigma, T);
+	double price_down = monte_carlo_call(paths, S - eps, K, r, sigma, T);
+
+	return (price_up - price_down) / (2.0 * eps);
+	
+}
 
 
 int main()
@@ -76,20 +134,49 @@ int main()
 	double r = 0.05;
 	double sigma = 0.2;
 	double T = 1.0;
-	int steps = 1000;
+	int steps = 10000;
+
+
+	double se;
+	double price = monte_carlo_call_stats(steps, S, K, r, sigma, T, se);
+
+	double discounted_se = std::exp(-r * T) * se;
+	std::cout << "Price: " << price << std::endl;
+	std::cout << "95% CI: ["
+		<< price - 1.96 * discounted_se << ", "
+		<< price + 1.96 * discounted_se << "]"
+		<< std::endl;
 
 	//std::cout << "Monte Carlo: " << mc << std::endl;
 	//std::cout << "Black-Scholes: " << bs << std::endl;
 	//std::cout << "Abs error: " << std::abs(mc - bs) << endl;
-	std::cout << "Paths\tMCPrice\tError\t\tError*sqrt(N)" << std::endl;
-	for (int i=3;i<=6;++i)
+	
+	std::cout << std::fixed << std::setprecision(6);
+	std::cout << std::setw(10) << "Paths"
+			  << std::setw(15) << "MC"
+			  << std::setw(15) << "Error"
+			  << std::setw(20) << "ScaledErr"
+			  << std::setw(15) << "AntiMC"
+			  << std::setw(15) << "AntiErr"
+			  << std::endl;
+	
+	double bs = black_scholes_call(S, K, r, sigma, T);
+	for (int i=3;i<=7;++i)
 	{
-		int paths = std::pow(10,i);
+		int paths = std::pow(10, i);
+		double amc = monte_carlo_call_antithetic(paths, S, K, r, sigma, T);
 		double mc = monte_carlo_call(paths, S, K, r, sigma, T);
-		double bs = black_scholes_call(S, K, r, sigma, T);
 		double error = std::abs(mc - bs);
 
-		std::cout << paths <<"\t" << mc << "\t" << error <<"\t"<<error*std::sqrt(paths) << std::endl;
+		double antierror = std::abs(amc - bs);
+
+		std::cout << std::setw(10) << paths
+			 	  << std::setw(15) << mc
+			      << std::setw(15) << error
+				  << std::setw(20) << error * std::sqrt(paths)
+				  << std::setw(15) << amc
+				  << std::setw(15) << antierror
+				  << std::endl;
 	}
 
 	return 0;
